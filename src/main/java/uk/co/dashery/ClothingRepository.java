5060a47b77a78e5ec14479cd9de9c152937f65aa
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-11-15 15:44:32 +0000

    Removed config file in favour of environment variables.

--- src/main/java/uk/co/dashery/ClothingRepository.java
+++ src/main/java/uk/co/dashery/ClothingRepository.java
@@ -3,6 +3,7 @@ package uk.co.dashery;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
