93ee4557fc1515d2a957cfea63ad4b5502307302
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-11-15 12:19:56 +0000

    Checking properties are working...

--- src/main/java/uk/co/dashery/DasheryClothingQueryApplication.java
+++ src/main/java/uk/co/dashery/DasheryClothingQueryApplication.java
@@ -1,10 +1,27 @@
 package uk.co.dashery;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@RestController
 public class DasheryClothingQueryApplication {
+
+
+
+    @Value("${spring.data.mongodb.uri}")
+    String name = "World";
+
+    @RequestMapping("/")
+    public String home() {
+       return "Hello " + name;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DasheryClothingQueryApplication.class, args);
