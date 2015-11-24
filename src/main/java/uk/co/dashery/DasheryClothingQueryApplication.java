f072c8e38f28dbcfa9ee3f7ed9315b14214afcb0
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-11-24 22:13:06 +0000

    Added RestTemplate bean to be autowired.

--- src/main/java/uk/co/dashery/DasheryClothingQueryApplication.java
+++ src/main/java/uk/co/dashery/DasheryClothingQueryApplication.java
@@ -2,9 +2,16 @@ package uk.co.dashery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DasheryClothingQueryApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DasheryClothingQueryApplication.class, args);
