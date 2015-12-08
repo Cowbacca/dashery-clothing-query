d17e25205061e7ed7363592f6b5943d1c899e86b
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-08 20:59:33 +0000

    Cleaned up tests and added test for ensuring execute and revert are opposites.

--- src/test/java/uk/co/dashery/service/DelimitedStringToSetConversionTest.java
+++ src/test/java/uk/co/dashery/service/DelimitedStringToSetConversionTest.java
@@ -4,26 +4,44 @@ import com.google.common.collect.Sets;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 
 public class DelimitedStringToSetConversionTest {
 
+    public static final String A_TOKEN = "test";
+    public static final String ANOTHER_TOKEN = "another";
+    public static final String DELIMITER = ";";
     private DelimitedStringToSetConversion delimitedStringToSetConversion;
 
     @Before
     public void setUp() throws Exception {
-        delimitedStringToSetConversion = new DelimitedStringToSetConversion(";");
+        delimitedStringToSetConversion = new DelimitedStringToSetConversion(DELIMITER);
     }
 
     @Test
     public void testExecute() throws Exception {
-        assertThat(delimitedStringToSetConversion.execute("test;another"), is(Sets.newHashSet("test", "another")));
+        String delimitedString = generateDelimitedString(DELIMITER);
+        assertThat(delimitedStringToSetConversion.execute(delimitedString), is(Sets.newHashSet(A_TOKEN, ANOTHER_TOKEN)));
+    }
+
+    private String generateDelimitedString(String delimiter) {
+        return A_TOKEN + delimiter + ANOTHER_TOKEN;
     }
 
     @Test
     public void testRevert() throws Exception {
-        assertThat(delimitedStringToSetConversion.revert(Sets.newHashSet("test", "another")), is("test;another"));
+        String delimitedString = generateDelimitedString(DELIMITER);
+        assertThat(delimitedStringToSetConversion.revert(Sets.newHashSet(A_TOKEN, ANOTHER_TOKEN)), is(delimitedString));
+    }
+
+    @Test
+    public void testExecuteAndRevertAreOpposites() {
+        String delimitedString = generateDelimitedString(DELIMITER);
+        Set<String> stringSet = delimitedStringToSetConversion.execute(delimitedString);
+        assertThat(delimitedStringToSetConversion.revert(stringSet), is(delimitedString));
     }
 }
