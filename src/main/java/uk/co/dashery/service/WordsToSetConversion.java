9df60d63ef31234c2fdbaaeca1d46b4e36767e75
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-08 19:19:28 +0000

    Simplified WordtoSetConversion.

--- src/main/java/uk/co/dashery/service/WordsToSetConversion.java
+++ src/main/java/uk/co/dashery/service/WordsToSetConversion.java
@@ -1,11 +1,9 @@
 package uk.co.dashery.service;
 
+import com.google.common.collect.Sets;
 import com.univocity.parsers.conversions.Conversion;
 
-import java.util.Collections;
 import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 /**
  * Modified from https://github.com/uniVocity/univocity-parsers
@@ -28,20 +26,11 @@ public class WordsToSetConversion implements Conversion<String, Set<String>> {
 
     @Override
     public Set<String> execute(String input) {
-        if (input == null) {
-            return Collections.emptySet();
-        }
-
-        Set<String> out = new TreeSet<String>();
-        for (String token : input.split(separator)) {
-            out.add(token);
-        }
-
-        return out;
+        return Sets.newHashSet(input.split(separator));
     }
 
     @Override
-    public String revert(Set<String> o) {
-        return o.stream().collect(Collectors.joining(separator));
+    public String revert(Set<String> set) {
+        return String.join(separator, set);
     }
 }
