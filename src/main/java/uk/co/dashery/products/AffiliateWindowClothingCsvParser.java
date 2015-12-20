277b5bb916f323897954697b188d42ca9ee85c37
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-20 20:15:13 +0000

    Added product_name to name mapping.

--- src/main/java/uk/co/dashery/products/AffiliateWindowClothingCsvParser.java
+++ src/main/java/uk/co/dashery/products/AffiliateWindowClothingCsvParser.java
@@ -27,6 +27,7 @@ public class AffiliateWindowClothingCsvParser extends ClothingCsvParser<RowListP
         columnNameMap.put("search_price", (clothing, price) -> clothing.price = Integer.parseInt(price));
         columnNameMap.put("description", (clothing, description) -> clothing.tags = new HashSet<>());
         columnNameMap.put("merchant_name", (clothing, brand) -> clothing.brand = brand);
+        columnNameMap.put("product_name", (clothing, name) -> clothing.name = name);
     }
 
     @Override
--- src/test/java/uk/co/dashery/products/ProductsServiceTest.java
+++ src/test/java/uk/co/dashery/products/ProductsServiceTest.java
@@ -42,8 +42,8 @@ public class ProductsServiceTest {
     public void testParsesCsvInAffiliateWindowFormat() throws IOException {
         List<Clothing> products = productsService.getClothingFrom(new Products(generateCsvFile("affiliatewindow.csv"), true));
 
-        List<Clothing> expectedProducts = Lists.newArrayList(generateClothing("A Test Brand", null, 100),
-                generateClothing("Another Day", null, 200));
+        List<Clothing> expectedProducts = Lists.newArrayList(generateClothing("A Test Brand", "Test Item", 100),
+                generateClothing("Another Day", "Another Dollar", 200));
         assertThat(products, is(expectedProducts));
     }
 }
