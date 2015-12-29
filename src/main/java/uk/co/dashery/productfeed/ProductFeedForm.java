edeb8f23dfbe25153ae2df4c4cb35fce3f579a71
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-29 16:14:30 +0000

    Replaced generated code in ProductFeedForm with Lombok annotations.

--- src/main/java/uk/co/dashery/productfeed/ProductFeedForm.java
+++ src/main/java/uk/co/dashery/productfeed/ProductFeedForm.java
@@ -1,10 +1,14 @@
 package uk.co.dashery.productfeed;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.net.URL;
 
+@Data
+@NoArgsConstructor
 public class ProductFeedForm {
 
     private boolean usingUrl;
@@ -13,9 +17,6 @@ public class ProductFeedForm {
     private MultipartFile file;
 
     private boolean isAffiliateWindowFormat;
-
-    public ProductFeedForm() {
-    }
 
     public ProductFeedForm(String url) {
         this.usingUrl = true;
@@ -30,60 +31,6 @@ public class ProductFeedForm {
     public ProductFeedForm(MultipartFile file, boolean isAffiliateWindowFormat) {
         this.file = file;
         this.isAffiliateWindowFormat = isAffiliateWindowFormat;
-    }
-
-    public boolean isUsingUrl() {
-        return usingUrl;
-    }
-
-    public void setUsingUrl(boolean usingUrl) {
-        this.usingUrl = usingUrl;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public MultipartFile getFile() {
-        return file;
-    }
-
-    public void setFile(MultipartFile file) {
-        this.file = file;
-    }
-
-    public boolean isAffiliateWindowFormat() {
-        return isAffiliateWindowFormat;
-    }
-
-    public void setAffiliateWindowFormat(boolean isAffiliateWindowFormat) {
-        this.isAffiliateWindowFormat = isAffiliateWindowFormat;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ProductFeedForm productFeedForm = (ProductFeedForm) o;
-
-        if (usingUrl != productFeedForm.usingUrl) return false;
-        if (url != null ? !url.equals(productFeedForm.url) : productFeedForm.url != null)
-            return false;
-        return !(file != null ? !file.equals(productFeedForm.file) : productFeedForm.file != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (usingUrl ? 1 : 0);
-        result = 31 * result + (url != null ? url.hashCode() : 0);
-        result = 31 * result + (file != null ? file.hashCode() : 0);
-        return result;
     }
 
     public Reader generateReader() throws IOException {
