2356fc8dd22c4a2c33bdb84edc3e41b16885eb07
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-30 16:38:32 +0000

    Removed event listener.

--- src/main/java/uk/co/dashery/clothing/ClothingController.java
+++ src/main/java/uk/co/dashery/clothing/ClothingController.java
@@ -1,12 +1,10 @@
 package uk.co.dashery.clothing;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import uk.co.dashery.productfeed.ProductsCreatedEvent;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -23,12 +21,6 @@ public class ClothingController {
     @RequestMapping
     public List<Clothing> clothing(@RequestParam String search) {
         return clothingService.search(search);
-    }
-
-    @EventListener
-    public void handleProductsCreated(ProductsCreatedEvent productsCreatedEvent) {
-        List<Clothing> clothingList = clothingFactory.create(productsCreatedEvent.getProducts());
-        clothingService.create(clothingList);
     }
 
     @RabbitListener(queues = "products")
