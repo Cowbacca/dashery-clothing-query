6bb927ed5e3ebe04f413ccd41c185ab707739a42
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-30 14:36:27 +0000

    Implemented full conversion.

--- src/main/java/uk/co/dashery/clothing/rabbitmq/ClothingJsonMessageConverter.java
+++ src/main/java/uk/co/dashery/clothing/rabbitmq/ClothingJsonMessageConverter.java
@@ -1,5 +1,6 @@
 package uk.co.dashery.clothing.rabbitmq;
 
+import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import com.google.gson.internal.LinkedTreeMap;
 import org.springframework.amqp.core.Message;
@@ -8,7 +9,13 @@ import org.springframework.amqp.support.converter.MessageConversionException;
 import org.springframework.amqp.support.converter.MessageConverter;
 import uk.co.dashery.clothing.Clothing;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ClothingJsonMessageConverter implements MessageConverter {
+
     @Override
     public Message toMessage(Object o, MessageProperties messageProperties) throws
             MessageConversionException {
@@ -16,10 +23,33 @@ public class ClothingJsonMessageConverter implements MessageConverter {
     }
 
     @Override
-    public Clothing fromMessage(Message message) throws MessageConversionException {
+    public List<Clothing> fromMessage(Message message) throws MessageConversionException {
         Gson gson = new Gson();
         String json = new String(message.getBody());
         LinkedTreeMap[] jsonMaps = gson.fromJson(json, LinkedTreeMap[].class);
-        return new Clothing((String) jsonMaps[0].get("id"));
+        return Arrays.stream(jsonMaps)
+                .map(this::getClothing)
+                .collect(Collectors.toList());
+    }
+
+    private Clothing getClothing(LinkedTreeMap jsonMap) {
+        Clothing clothing = new Clothing();
+        clothing.setId((String) jsonMap.get("id"));
+        clothing.setBrand((String) jsonMap.get("merchant"));
+        clothing.setName((String) jsonMap.get("name"));
+        clothing.setPrice(((Double) jsonMap.get("price")).intValue());
+        clothing.setLink((String) jsonMap.get("link"));
+        clothing.setImageLink((String) jsonMap.get("imageLink"));
+        clothing.setTags(getTags((String) jsonMap.get("description")));
+        return clothing;
+    }
+
+    private HashSet<String> getTags(String description) {
+        if (description != null) {
+            String[] tags = description.split(" ");
+            return Sets.newHashSet(tags);
+        } else {
+            return Sets.newHashSet();
+        }
     }
 }
--- src/test/java/uk/co/dashery/clothing/rabbitmq/ClothingJsonMessageConverterTest.java
+++ src/test/java/uk/co/dashery/clothing/rabbitmq/ClothingJsonMessageConverterTest.java
@@ -1,27 +1,63 @@
 package uk.co.dashery.clothing.rabbitmq;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import uk.co.dashery.clothing.Clothing;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ClothingJsonMessageConverterTest {
 
+    private ClothingJsonMessageConverter clothingJsonMessageConverter;
+
+    @Before
+    public void setUp() {
+        clothingJsonMessageConverter = new ClothingJsonMessageConverter();
+    }
+
     @Test
-    public void testFromMessage() throws Exception {
-        ClothingJsonMessageConverter clothingJsonMessageConverter = new
-                ClothingJsonMessageConverter();
+    public void testConvertsFromJsonMessage() throws Exception {
+        Message message = givenAMessageWithTwoJsonProducts();
 
-        String productsJson = "[{\"id\":\"id123\",\"merchant\":\"A Test Brand\",\"name\":\"Test " +
-                "Item\",\"description\":\"Some description or other.\",\"price\":10000," +
-                "\"link\":\"a_link.html\",\"imageLink\":\"image.jpg\"}]";
-        Message message = new Message(productsJson.getBytes(), new MessageProperties());
+        List<Clothing> convertedClothing = clothingJsonMessageConverter.fromMessage(message);
 
-        Clothing convertedClothing = clothingJsonMessageConverter.fromMessage(message);
+        ArrayList<Clothing> expectedClothing = expectedClothing();
+        assertThat(convertedClothing, is(expectedClothing));
+    }
 
-        assertThat(convertedClothing, is(new Clothing("id123")));
+    private Message givenAMessageWithTwoJsonProducts() {
+        String productsJson = "[" +
+                "{\"id\":\"id123\"," +
+                "\"merchant\":\"A Test Brand\"," +
+                "\"name\":\"Test Item\"," +
+                "\"description\":\"Some description or other.\"," +
+                "\"price\":10000," +
+                "\"link\":\"a_link.html\"," +
+                "\"imageLink\":\"image.jpg\"}," +
+                "{\"id\":\"id456\"," +
+                "\"merchant\":\"Another Day\"," +
+                "\"name\":\"Another Dollar\"," +
+                "\"description\":\"A different description.\"" +
+                ",\"price\":200," +
+                "\"link\":\"different_link\"," +
+                "\"imageLink\":\"image2.jpg\"}" +
+                "]";
+        return new Message(productsJson.getBytes(), new MessageProperties());
+    }
+
+    private ArrayList<Clothing> expectedClothing() {
+        Clothing firstClothing = new Clothing("id123", "A Test Brand", "Test Item", 10000,
+                "a_link.html", "image.jpg", Sets.newHashSet("description", "Some", "or", "other."));
+        Clothing secondClothing = new Clothing("id456", "Another Day", "Another Dollar", 200,
+                "different_link", "image2.jpg", Sets.newHashSet("A", "different", "description."));
+        return Lists.newArrayList(firstClothing, secondClothing);
     }
 }
