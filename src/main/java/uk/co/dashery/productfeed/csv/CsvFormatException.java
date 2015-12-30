e1464ef2a06977591c358fb792793ea6c8fe8930
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-30 16:37:14 +0000

    Removed product feed package from clothing query service.

--- src/main/java/uk/co/dashery/productfeed/csv/CsvFormatException.java
+++
@@ -1,7 +0,0 @@
-package uk.co.dashery.productfeed.csv;
-
-public class CsvFormatException extends RuntimeException {
-    public CsvFormatException(String message) {
-        super(message);
-    }
-}
