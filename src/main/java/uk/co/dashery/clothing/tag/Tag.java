fb1495c33a3ebbf0c264b8e815a351c009b7da44
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-31 16:19:37 +0000

    Removed unnecessary Document annotation.

--- src/main/java/uk/co/dashery/clothing/tag/Tag.java
+++ src/main/java/uk/co/dashery/clothing/tag/Tag.java
@@ -3,12 +3,10 @@ package uk.co.dashery.clothing.tag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
 public class Tag {
     private String value;
     private int score;
